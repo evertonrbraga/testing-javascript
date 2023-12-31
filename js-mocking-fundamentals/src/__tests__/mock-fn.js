564a04a7df8b33c890eb1a8ec1d961c07d365ff5
Everton Braga <evertonr.braga@gmail.com>
2023-12-31 12:01:18 -0300

    mocking test examples

--- js-mocking-fundamentals/src/__tests__/mock-fn.js
+++ js-mocking-fundamentals/src/__tests__/mock-fn.js
@@ -1,18 +1,42 @@
 const thumbWar = require("../thumb-war");
 const utils = require("../utils");
 
-jest.mock("../utils");
+// in an ES modules situation, that's the right implementation
+// i am importing the full implementation at __mocks__ folder
+jest.mock("../utils.js");
 
 test("returns winner", () => {
-  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler"); //esta funcao atribuiu valor a utils.getWinner
+  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
   expect(winner).toBe("Kent C. Dodds");
   expect(utils.getWinner).toHaveBeenCalledTimes(2);
-  expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler"); //isso é uma verificação de que a funcao precisa ter 2 parametros (mock ou spy)
+  expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler");
   expect(utils.getWinner.mock.calls).toEqual([
     ["Kent C. Dodds", "Ken Wheeler"],
     ["Kent C. Dodds", "Ken Wheeler"],
   ]);
 
-  // cleanup
+  utils.getWinner.mockReset();
+});
+
+/*
+-------FIRST IMPLEMENTATION-------
+
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
+
+test("returns winner", () => {
+  jest.spyOn(utils, "getWinner");
+  utils.getWinner.mockImplementation((p1, p2) => p1);
+
+  const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+  expect(winner).toBe("Kent C. Dodds");
+  expect(utils.getWinner).toHaveBeenCalledTimes(2);
+  expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler");
+  expect(utils.getWinner.mock.calls).toEqual([
+    ["Kent C. Dodds", "Ken Wheeler"],
+    ["Kent C. Dodds", "Ken Wheeler"],
+  ]);
+
   utils.getWinner.mockRestore();
 });
+*/
