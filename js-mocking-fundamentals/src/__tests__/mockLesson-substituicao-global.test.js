591cb4769fb6df330d6a64081da49d86298e0bee
Everton Braga <evertonr.braga@gmail.com>
2024-03-02 20:25:19 -0300

    jest-cypress-react-babel-webpack module initial commit

--- js-mocking-fundamentals/src/__tests__/mockLesson-substituicao-global.test.js
+++ js-mocking-fundamentals/src/__tests__/mockLesson-substituicao-global.test.js
@@ -17,9 +17,12 @@ jest.mock("../externals/dateUtils", () => ({
 }));
 
 test("Exemplo de teste utilizando dateUtils", () => {
+  const date = new Date("2022-01-01T00:00:00.000Z");
   // O módulo dateUtils agora está completamente substituído por versões mockadas
   // e você pode controlar o comportamento dessas funções durante o teste.
 
   // Por exemplo, getCurrentDate sempre retornará a data fixa '2022-01-01T00:00:00.000Z'
-  expect(getCurrentDate()).toEqual(new Date("2022-01-01T00:00:00.000Z"));
+  expect(getCurrentDate()).toEqual(new Date(date));
+  expect(formatToISO(date)).toEqual("2022-01-01T00:00:00.000Z");
+  expect(addDays(date, 2)).toEqual(new Date("2022-01-03T00:00:00.000Z"));
 });
